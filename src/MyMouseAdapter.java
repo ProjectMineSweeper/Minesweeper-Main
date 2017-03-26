146daa90017e0902f552a82c90aeeaa7e9fb6e5c
Ariel <Ariel@Neil-Tyson>
2017-03-26 00:44:12 -0400

    Añadi que cuando haces right click pinta el cuadrado de rojo. Cambié el panel a como deberia verse.

--- src/MyMouseAdapter.java
+++ src/MyMouseAdapter.java
@@ -8,7 +8,6 @@ import java.util.Random;
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
-	private Random generator = new Random();
 	public void mousePressed(MouseEvent e) {
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
@@ -34,6 +33,26 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
+			Component b = e.getComponent();
+			while (!(b instanceof JFrame)) {
+				b = b.getParent();
+				if (b == null) {
+					return;
+				}
+			}
+			JFrame myOtherFrame = (JFrame)b;
+			MyPanel myOtherPanel = (MyPanel) myOtherFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+			Insets myOtherInsets = myOtherFrame.getInsets();
+			int X1 = myOtherInsets.left;
+			int Y1 = myOtherInsets.top;
+			e.translatePoint(-X1, -Y1);
+			int X = e.getX();
+			int Y = e.getY();
+			myOtherPanel.x = X;
+			myOtherPanel.y = Y;
+			myOtherPanel.mouseDownGridX = myOtherPanel.getGridX(X, Y);
+			myOtherPanel.mouseDownGridY = myOtherPanel.getGridY(X, Y);
+			myOtherPanel.repaint();
 			//Do nothing
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
@@ -76,37 +95,79 @@ public class MyMouseAdapter extends MouseAdapter {
 						//Do nothing
 					} else {
 						//Released the mouse button on the same cell where it was pressed
-						if ((gridX == 0) || (gridY == 0)) {
-							//On the left column and on the top row... do nothing
-						} else {
-							//On the grid other than on the left column and on the top row:
-							Color newColor = null;
-							switch (generator.nextInt(5)) {
-							case 0:
-								newColor = Color.YELLOW;
-								break;
-							case 1:
-								newColor = Color.MAGENTA;
-								break;
-							case 2:
-								newColor = Color.BLACK;
-								break;
-							case 3:
-								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							case 4:
-								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
-								break;
-							}
-							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
-							myPanel.repaint();
-						}
+						myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.gray ;
+
+
+
+//						if ((gridX == 0) || (gridY == 0)) {
+//							//On the left column and on the top row... do nothing
+//						} else {
+//							//On the grid other than on the left column and on the top row:
+//							Color newColor = null;
+//							switch (generator.nextInt(5)) {
+//							case 0:
+//								newColor = Color.YELLOW;
+//								break;
+//							case 1:
+//								newColor = Color.MAGENTA;
+//								break;
+//							case 2:
+//								newColor = Color.BLACK;
+//								break;
+//							case 3:
+//								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
+//								break;
+//							case 4:
+//								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
+//								break;
+//							}
+//							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
+//							myPanel.repaint();
+//						}
 					}
 				}
 			}
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
+			Component c1 = e.getComponent();
+			while (!(c1 instanceof JFrame)) {
+				c1 = c1.getParent();
+				if (c1 == null) {
+					return;
+				}
+			}
+			JFrame myOtherFrame = (JFrame)c1;
+			MyPanel myOtherPanel = (MyPanel) myOtherFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
+			Insets myOtherInsets = myOtherFrame.getInsets();
+			int X1 = myOtherInsets.left;
+			int Y1 = myOtherInsets.top;
+			e.translatePoint(-X1, -Y1);
+			int X = e.getX();
+			int Y = e.getY();
+			myOtherPanel.x = X;
+			myOtherPanel.y = Y;
+			int otherGridX = myOtherPanel.getGridX(X, Y);
+			int otherGridY = myOtherPanel.getGridY(X, Y);
+			if(myOtherPanel.mouseDownGridX == otherGridX && myOtherPanel.mouseDownGridY == otherGridY){
+				myOtherPanel.colorArray[otherGridX][otherGridY] = Color.RED ;
+
+			}
+			else if ((myOtherPanel.mouseDownGridX == -1) || (myOtherPanel.mouseDownGridY == -1)) {
+				//Had pressed outside
+				//Do nothing
+			} else {
+				if ((otherGridX == -1) || (otherGridY == -1)) {
+					//Is releasing outside
+					//Do nothing
+				} else {
+					if ((myOtherPanel.mouseDownGridX != otherGridX) || (myOtherPanel.mouseDownGridY != otherGridY)) {
+						//Released the mouse button on a different cell where it was pressed
+						//Do nothing
+					}
+				}
+			}	
+			myOtherPanel.repaint();
 			//Do nothing
 			break;
 		default:    //Some other button (2 = Middle mouse button, etc.)
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -7,11 +7,11 @@ import javax.swing.JPanel;
 
 public class MyPanel extends JPanel {
 	private static final long serialVersionUID = 3426940946811133635L;
-	private static final int GRID_X = 25;
-	private static final int GRID_Y = 25;
-	private static final int INNER_CELL_SIZE = 29;
-	private static final int TOTAL_COLUMNS = 10;
-	private static final int TOTAL_ROWS = 11;   //Last row has only one cell
+	private static final int GRID_X = 55;
+	private static final int GRID_Y = 55;
+	private static final int INNER_CELL_SIZE = 40;
+	private static final int TOTAL_COLUMNS = 9;
+	private static final int TOTAL_ROWS = 10;   //Last row has only one cell
 	public int x = -1;
 	public int y = -1;
 	public int mouseDownGridX = 0;
@@ -27,14 +27,14 @@ public class MyPanel extends JPanel {
 		if (TOTAL_ROWS + (new Random()).nextInt(1) < 3) {	//Use of "random" to prevent unwanted Eclipse warning
 			throw new RuntimeException("TOTAL_ROWS must be at least 3!");
 		}
-		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
-			colorArray[x][0] = Color.LIGHT_GRAY;
-		}
-		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
-			colorArray[0][y] = Color.LIGHT_GRAY;
-		}
-		for (int x = 1; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
-			for (int y = 1; y < TOTAL_ROWS; y++) {
+//		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //Top row
+//			colorArray[x][0] = Color.LIGHT_GRAY;
+//		}
+//		for (int y = 0; y < TOTAL_ROWS; y++) {   //Left column
+//			colorArray[0][y] = Color.LIGHT_GRAY;
+//		}
+		for (int x = 0; x < TOTAL_COLUMNS; x++) {   //The rest of the grid
+			for (int y = 0; y < TOTAL_ROWS; y++) {
 				colorArray[x][y] = Color.WHITE;
 			}
 		}
@@ -66,11 +66,11 @@ public class MyPanel extends JPanel {
 		}
 
 		//Draw an additional cell at the bottom left
-		g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
+//		g.drawRect(x1 + GRID_X, y1 + GRID_Y + ((INNER_CELL_SIZE + 1) * (TOTAL_ROWS - 1)), INNER_CELL_SIZE + 1, INNER_CELL_SIZE + 1);
 
 		//Paint cell colors
 		for (int x = 0; x < TOTAL_COLUMNS; x++) {
-			for (int y = 0; y < TOTAL_ROWS; y++) {
+			for (int y = 0; y < TOTAL_ROWS - 1; y++) {
 				if ((x == 0) || (y != TOTAL_ROWS - 1)) {
 					Color c = colorArray[x][y];
 					g.setColor(c);
@@ -97,7 +97,7 @@ public class MyPanel extends JPanel {
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
 		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
-			return x;
+			return -1;
 		}
 		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
 			return -1;
@@ -122,7 +122,7 @@ public class MyPanel extends JPanel {
 		x = x / (INNER_CELL_SIZE + 1);
 		y = y / (INNER_CELL_SIZE + 1);
 		if (x == 0 && y == TOTAL_ROWS - 1) {    //The lower left extra cell
-			return y;
+			return -1;
 		}
 		if (x < 0 || x > TOTAL_COLUMNS - 1 || y < 0 || y > TOTAL_ROWS - 2) {   //Outside the rest of the grid
 			return -1;
