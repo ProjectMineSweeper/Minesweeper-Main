d9abc800293db263db0c389005205718cda0bbcf
LauraRosario <laura.rosario1@upr.edu>
2017-03-26 21:01:49 -0400

    Update MyMouseAdapter.java

--- src/MyMouseAdapter.java
+++ src/MyMouseAdapter.java
@@ -4,7 +4,6 @@ import java.awt.Insets;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.Random;
-
 import javax.swing.JFrame;
 
 public class MyMouseAdapter extends MouseAdapter {
@@ -31,7 +30,8 @@ public class MyMouseAdapter extends MouseAdapter {
 			myPanel.mouseDownGridX = myPanel.getGridX(x, y);
 			myPanel.mouseDownGridY = myPanel.getGridY(x, y);
 			myPanel.repaint();
-			break;
+			break;	
+			
 		case 3:		//Right mouse button
 			Component b = e.getComponent();
 			while (!(b instanceof JFrame)) {
@@ -55,11 +55,16 @@ public class MyMouseAdapter extends MouseAdapter {
 			myOtherPanel.repaint();
 			//Do nothing
 			break;
+	
+			
+			
+	        
 		default:    //Some other button (2 = Middle mouse button, etc.)
 			//Do nothing
 			break;
 		}
 	}
+		
 	public void mouseReleased(MouseEvent e) {
 		switch (e.getButton()) {
 		case 1:		//Left mouse button
@@ -85,6 +90,8 @@ public class MyMouseAdapter extends MouseAdapter {
 			if ((myPanel.mouseDownGridX == -1) || (myPanel.mouseDownGridY == -1)) {
 				//Had pressed outside
 				//Do nothing
+				
+				
 			} else {
 				if ((gridX == -1) || (gridY == -1)) {
 					//Is releasing outside
@@ -93,32 +100,39 @@ public class MyMouseAdapter extends MouseAdapter {
 					if ((myPanel.mouseDownGridX != gridX) || (myPanel.mouseDownGridY != gridY)) {
 						//Released the mouse button on a different cell where it was pressed
 						//Do nothing
+						
 					} else {
-						//Released the mouse button on the same cell where it was pressed
-						myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = Color.gray ;
-
-
-
+						
+						
+						myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY].equals(Color.gray) ;
+						
+						break;
+						
+					}else{
+			
+						if(myPanel.isMine(myPanel.mouseDownGridX, myPanel.mouseDownGridY)){
+							Color newColor = Color.BLACK;
+							for(int i = 0; i<9; i++){
+								for (int j=0; j < 9; j++){
+									if (myPanel.minesPanel[i][j]==true){
+										
+										myPanel.colorArray[i][j]=newColor;
+										
+										myPanel.repaint();
+									}
+								}
+							}
+						}
+					
+					
 //						if ((gridX == 0) || (gridY == 0)) {
 //							//On the left column and on the top row... do nothing
-//						} else {
+//						
 //							//On the grid other than on the left column and on the top row:
 //							Color newColor = null;
 //							switch (generator.nextInt(5)) {
 //							case 0:
-//								newColor = Color.YELLOW;
-//								break;
-//							case 1:
-//								newColor = Color.MAGENTA;
-//								break;
-//							case 2:
 //								newColor = Color.BLACK;
-//								break;
-//							case 3:
-//								newColor = new Color(0x964B00);   //Brown (from http://simple.wikipedia.org/wiki/List_of_colors)
-//								break;
-//							case 4:
-//								newColor = new Color(0xB57EDC);   //Lavender (from http://simple.wikipedia.org/wiki/List_of_colors)
 //								break;
 //							}
 //							myPanel.colorArray[myPanel.mouseDownGridX][myPanel.mouseDownGridY] = newColor;
@@ -127,17 +141,18 @@ public class MyMouseAdapter extends MouseAdapter {
 					}
 				}
 			}
+		
 			myPanel.repaint();
 			break;
 		case 3:		//Right mouse button
-			Component c1 = e.getComponent();
-			while (!(c1 instanceof JFrame)) {
-				c1 = c1.getParent();
-				if (c1 == null) {
+			Component b = e.getComponent();
+			while (!(b instanceof JFrame)) {
+				b = b.getParent();
+				if (b == null) {
 					return;
 				}
 			}
-			JFrame myOtherFrame = (JFrame)c1;
+			JFrame myOtherFrame = (JFrame)b;
 			MyPanel myOtherPanel = (MyPanel) myOtherFrame.getContentPane().getComponent(0);  //Can also loop among components to find MyPanel
 			Insets myOtherInsets = myOtherFrame.getInsets();
 			int X1 = myOtherInsets.left;
@@ -175,4 +190,8 @@ public class MyMouseAdapter extends MouseAdapter {
 			break;
 		}
 	}
-}
+		
+	}
+	
+
+
